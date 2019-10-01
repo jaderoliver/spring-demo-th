2519b7d921d2d732c4258efb822e0e292ead98e4
Willes Reis <willes.reis@cda.sp.gov.br>
2019-10-01 14:46:10 -0300

    incluído formulário para teste de encode.

--- src/main/java/com/example/springdemo/web/WelcomeController.java
+++ src/main/java/com/example/springdemo/web/WelcomeController.java
@@ -1,8 +1,11 @@
 package com.example.springdemo.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * @author willesreis
@@ -15,10 +18,22 @@ public class WelcomeController {
         return "index";
     }
 
+    @GetMapping("/form")
+    public String form() {
+        return "form";
+    }
+
     @GetMapping("/test")
     public ModelAndView welcome() {
         ModelAndView view = new ModelAndView("welcome");
         view.addObject("message", "Hello World!");
+        return view;
+    }
+
+    @PostMapping("/test-encode")
+    public ModelAndView encode(@RequestBody MultiValueMap<String, String> formData) {
+        ModelAndView view = new ModelAndView("form");
+        view.addObject("txtarea", formData.get("txtarea").get(0));
         return view;
     }
 
